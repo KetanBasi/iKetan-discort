efca6c1d28f942b06a1099446582e53785870db3
KetanBasi <fatanaqilla90@gmail.com>
2020-12-18 17:58:21 +0700

    modified:   main/main.py 	new file:   main/token

--- main/main.py
+++ main/main.py
@@ -2,6 +2,17 @@ import discord
 import discord.ext
 import components.modules
 
+# To Invite this bot to your server:
+# https://discordapp.com/oauth2/author/authorize?client_id=<Bot Client ID>&scope=bot&permission=8
+# Use link above, change the <Bot Client ID> with
+#  your Bot Client ID on Discord Developers page
+
+def read_sc_token():
+    with open('token', 'r') as token_file:
+        lines = f.readlines()
+        return lines[0].strip()
+
+token = read_sc_token()
 client = discord.Client()
 
 command_prefix = "$"
@@ -26,6 +37,6 @@ async def on_message(message):
         elif command == "knock":
             await message.channel.send("Who's there?")
 
-client.run()
+client.run(token)
 
 await client.change_presence(activity=discord.Game(name="KetanBot Beta Ver."))
