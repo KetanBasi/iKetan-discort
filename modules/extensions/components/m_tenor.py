2190d2e88d0fd51039d05f6a0c12dd0a5bedcabe
KetanBasi <fatanaqilla90@gmail.com>
2021-08-19 17:36:27 +0700

    0.2|feat|add new commands & restructure

---
+++ modules/extensions/components/m_tenor.py
@@ -0,0 +1,70 @@
+import json
+import random
+import typing
+import requests
+
+from modules.core.c_core import get_token
+
+
+# ? ==================
+# ? Local Variables
+# ? ==================
+
+tenor_token = get_token('tenor')
+tenor_api = 'https://g.tenor.com/v1/search?q={0}&key={1}&limit={2}' \
+            + '&media_filter=minimal'
+
+# DESC: "caching" results
+# TODO: Use database instead :)
+gifs = {}
+last_random = {}
+
+
+# ? ==================
+# ? Functions
+# ? ==================
+
+def _get(api, term, token, limit=20) -> requests.models.Response:
+    """
+    Make a request
+    """
+    r = requests.get(
+            api.format(term, token, limit)
+            )
+    return r
+
+
+def get_gif(term, limit: typing.Optional[int] = 20) -> str:
+    """
+    get gif and process it
+    """
+    result = _get(tenor_api, term, tenor_token, limit)
+    
+    if result.status_code == 200:
+        
+        while True:
+            i = random.randint(0, limit-1)
+            
+            if term in last_random:
+                
+                if last_random[term] == i:
+                    continue
+            
+            else:
+                last_random[term] = i
+            
+            break
+        
+        return json.loads(result.content)['results'][i]['media'][0]['gif']['url']
+    
+    return f'*GIF source currently unavailable*: {result.status_code}'
+
+
+def test() -> str:
+    """
+    test GIF API connection
+    """
+    term = "happy"
+    limit = 10
+
+    return get_gif(term, limit)
--- modules/extensions/x_fun.py
+++ modules/extensions/x_fun.py
@@ -4,7 +4,7 @@ import typing
 import discord
 from discord.ext import commands
 
-from modules.core import c_gif
+from modules.extensions.components import m_tenor, m_bored, m_age_nation
 
 
 class Fun(commands.Cog):
@@ -15,171 +15,303 @@ class Fun(commands.Cog):
     # ? ==================
     # ? Commands
     # ? ==================
-    @commands.command(aliases=['gifs', 'getgif', 'getgifs', 'findgif', 'findgifs'],
-                      descriptiom='Find a GIF')
-    async def gif(self,
-                  ctx,
-                  *,
-                  keyword):
+    @commands.command(
+        aliases=['gifs', 'getgif', 'getgifs', 'findgif', 'findgifs']
+        )
+    async def gif(
+            self, ctx,
+            *, keyword
+            ):
         """
         Get yourself a GIF
 
-        Command: `[p]gif <keyword>` where [p] is bot prefix,
-                or
-                `[p]gif <keyword>|[message]`
-        Example: `{0}gif happy birthday` or `{0}gif hugs`
+        ***Usage***:
+            `[p]gif <keyword>` where [p] is bot prefix, or
+            `[p]gif <keyword>|[message]`
+        ***Example***: `{0}gif hugs` or `{0}gif happy birthday|Happy birthday @Jane`
         """
-        if '|' in keyword:
-            message = keyword.split('|')[1].strip()
-            keyword = keyword.split('|')[0].strip()
-            embed = discord.Embed(description=message,
-                                  colour=discord.Colour.green()
-                                  )
-        else:
-            embed = discord.Embed(colour=discord.Colour.green())
-        embed.set_image(url=c_gif.getGif(keyword))
-        embed.set_footer(text=f'Requested by {ctx.message.author.display_name}',
-                         icon_url=ctx.message.author.avatar_url_as(size=128))
-        await ctx.send(embed=embed)
+        async with ctx.typing():
+            if '|' in keyword:
+                message = keyword.split('|')[1].strip()
+                keyword = keyword.split('|')[0].strip()
+                embed = discord.Embed(
+                    description=message,
+                    colour=discord.Colour.green()
+                    )
+            else:
+                embed = discord.Embed(colour=discord.Colour.green())
+            embed.set_image(url=m_tenor.get_gif(keyword))
+            embed.set_footer(
+                text=f'Requested by {ctx.message.author.display_name}',
+                icon_url=ctx.message.author.avatar_url_as(size=128)
+                )
+            await ctx.send(embed=embed)
 
     @commands.command(aliases=['hugs'],
                       description='Hug someone')
-    async def hug(self,
-                  ctx,
-                  user: typing.Optional[discord.User] = None
-                  ):
+    async def hug(
+            self, ctx,
+            user: typing.Optional[discord.User] = None
+            ):
         """
         Hugs someone
 
-        Command: `[p]hug [user]`
-        Example: `{0}hug <@someone#1234>`
+        ***Usage***: `[p]hug [user]`
+        ***Example***: `{0}hug <@someone#1234>`
         """
-        if user is not None:
-            description = f'{ctx.message.author.display_name} hugs <@!{user.id}>'
-        else:
-            description = f'I hug you, {ctx.message.author.display_name}'
-        message = discord.Embed(description=description,
-                                colour=discord.Colour.green()
-                                )
-        message.set_image(url=c_gif.getGif('hugs'))
-        message.set_footer(text=f'Requested by {ctx.message.author.display_name}',
-                           icon_url=ctx.message.author.avatar_url_as(size=128))
-        await ctx.send(embed=message)
+        async with ctx.typing():
+            if user is not None:
+                description = f'{ctx.message.author.display_name} hugs <@!{user.id}>'
+            
+            else:
+                description = f'I hug you, {ctx.message.author.display_name}'
+            
+            message = discord.Embed(
+                description=description,
+                colour=discord.Colour.green()
+                )
+            message.set_image(url=m_tenor.get_gif('hugs'))
+            message.set_footer(
+                text=f'Requested by {ctx.message.author.display_name}',
+                icon_url=ctx.message.author.avatar_url_as(size=128)
+                )
+            await ctx.send(embed=message)
 
     @commands.command(aliases=['slaps'],
                       description='Slap someone')
-    async def slap(self,
-                   ctx,
-                   user: typing.Optional[discord.User] = None
-                   ):
+    async def slap(
+            self, ctx,
+            user: typing.Optional[discord.User] = None
+            ):
         """
         Slaps someone
 
-        Command: `[p]slap [user]`
-        Example: `{0}slap <@someone#1234>`
+        ***Usage***: `[p]slap [user]`
+        ***Example***: `{0}slap <@someone#1234>`
         """
-        if user is not None:
-            description = f'{ctx.message.author.display_name} slaps <@!{user.id}>'
-        else:
-            description = f'I slap you, {ctx.message.author.display_name}!'
-        message = discord.Embed(description=description,
-                                colour=discord.Colour.green()
-                                )
-        message.set_image(url=c_gif.getGif('slaps'))
-        message.set_footer(text=f'Requested by {ctx.message.author.display_name}',
-                           icon_url=ctx.message.author.avatar_url_as(size=128))
-        await ctx.send(embed=message)
+        async with ctx.typing():
+            if user is not None:
+                description = f'{ctx.message.author.display_name} slaps <@!{user.id}>'
+            
+            else:
+                description = f'I slap you, {ctx.message.author.display_name}!'
+            
+            message = discord.Embed(
+                description=description,
+                colour=discord.Colour.green()
+                )
+            message.set_image(url=m_tenor.get_gif('slaps'))
+            message.set_footer(
+                text=f'Requested by {ctx.message.author.display_name}',
+                icon_url=ctx.message.author.avatar_url_as(size=128)
+                )
+            await ctx.send(embed=message)
 
-    @commands.command(aliases=['flipcoin'],
-                      description='Flip a coin'
-                      )
-    async def coin(self,
-                   ctx,
-                   count: typing.Optional[int] = 1
-                   ):
+    @commands.command(aliases=['flipcoin'], description='Flip a coin')
+    async def coin(
+            self, ctx,
+            count: typing.Optional[int] = 1
+            ):
         """
         Flip a coin
 
-        Command: `[p]coin <number>` where [p] is bot prefix
-        Example: `{0}coin 3` which means *flip a coin 3 times*
+        ***Usage***: `[p]coin <number>` where [p] is bot prefix
+        ***Example***: `{0}coin 3` which means *flip a coin 3 times*
         """
-        sides = ('head', 'tail')
-        results = []
-        if count >= 1:
-            for i in range(count):
-                results.append(random.choice(sides))
-            # await ctx.send(', '.join(results))
-            embed = discord.Embed(title='Coin Flip',
-                                  colour=discord.Colour.green()
-                                  )
-            embed.add_field(name='Result:',
-                            value=', '.join(results),
-                            inline=False
-                            )
-            if count >= 2:
-                headCount = results.count('head')
-                tailCount = results.count('tail')
-                count = f'> Head: {headCount}\n> Tail: {tailCount}'
-                embed.add_field(name='Count:',
-                                value=count,
-                                inline=False
-                                )
-            embed.set_footer(text=f'Flipped by {ctx.message.author.display_name}',
-                             icon_url=ctx.message.author.avatar_url_as(size=128)
-                             )
-            await ctx.send(embed=embed)
-        else:
-            await ctx.send('Invalid argument, please insert positive value more than **0**')
+        async with ctx.typing():
+            sides = ('head', 'tail')
+            
+            if count >= 1:
+                results = [random.choice(sides) for _ in range(count)]
+                # await ctx.send(', '.join(results))
+                embed = discord.Embed(
+                    title='Coin Flip',
+                    colour=discord.Colour.green()
+                    )
+                embed.add_field(
+                    name='Result:',
+                    value=', '.join(results),
+                    inline=False
+                    )
+                
+                if count >= 2:
+                    head_count = results.count('head')
+                    tail_count = results.count('tail')
+                    count = f'> Head: {head_count}\n> Tail: {tail_count}'
+                    embed.add_field(
+                        name='Count:',
+                        value=count,
+                        inline=False
+                        )
+                embed.set_footer(
+                    text=f'Flipped by {ctx.message.author.display_name}',
+                    icon_url=ctx.message.author.avatar_url_as(size=128)
+                    )
+                await ctx.send(embed=embed)
+            
+            else:
+                await ctx.send(
+                    "Invalid argument, please insert"
+                    "positive value more than **0**"
+                    )
 
-    @commands.command(aliases=['rolldice', 'diceroll'],
-                      description='Roll a dice')
-    async def dice(self,
-                   ctx,
-                   rolls: typing.Optional[int] = 1,
-                   diceSides: typing.Optional[int] = 6
-                   ):
+    @commands.command(
+            aliases=['rolldice', 'diceroll'],
+            description='Roll a dice'
+            )
+    async def dice(
+            self, ctx,
+            rolls: typing.Optional[int] = 1,
+            diceSides: typing.Optional[int] = 6
+            ):
         """
         Roll a dice
 
-        Command: `[p]dice <rolls> [diceSide]` where [p] is bot prefix
-        **rolls**: how many time you roll the dice
-        **diceSide**: how many side the dice has (default: 6)
-        Example:
+        ***Usage***: `[p]dice <rolls> [diceSide]` where [p] is bot prefix
+            __rolls__: how many time you roll the dice
+            __diceSide__: how many side the dice has (default: 6)
+        ***Example***:
             `{0}dice 3 12` which means *roll a dice with 12 sides 3 times*
             or
             `{0}dice 5` which means *roll a dice with 6 sides 5 times*
         """
-        if diceSides >= 2:
-            if 1 <= rolls <= 100:
-                embed = discord.Embed(title='Dice Rolls',
-                                      color=discord.Colour.orange()
-                                      )
-                results = [random.randint(1, diceSides) for _ in range(rolls)]
-                embed.add_field(name='Result:',
-                                value=', '.join([str(num) for num in results]),
-                                inline=False
-                                )
-                if rolls > 1:
-                    maxVal = max(results)
-                    mode = statistics.mode(results)
-                    stats = f'> Max: {maxVal}\n> Mode: {mode}'
-                    embed.add_field(name='Statistics:',
-                                    value=stats,
-                                    inline=False,
-                                    )
-                embed.set_footer(text=f'Dice rolls by {ctx.message.author.display_name}',
-                                 icon_url=ctx.message.author.avatar_url_as(size=128))
-                await ctx.send(embed=embed)
+        async with ctx.typing():
+            if diceSides >= 2:
+                if 1 <= rolls <= 100:
+                    results = [random.randint(1, diceSides) for _ in range(rolls)]
+                    embed = discord.Embed(
+                        title='Dice Rolls',
+                        color=discord.Colour.orange()
+                        )
+                    embed.add_field(
+                        name='Result:',
+                        value=', '.join([str(num) for num in results]),
+                        inline=False
+                        )
+                    
+                    if rolls > 1:
+                        max_val = max(results)
+                        mode = statistics.mode(results)
+                        stats = f'> Max: {max_val}\n> Mode: {mode}'
+                        embed.add_field(
+                            name='Statistics:',
+                            value=stats,
+                            inline=False,
+                            )
+                    
+                    embed.set_footer(
+                        text=f'Dice rolls by {ctx.message.author.display_name}',
+                        icon_url=ctx.message.author.avatar_url_as(size=128)
+                        )
+                    await ctx.send(embed=embed)
+                
+                else:
+                    await ctx.send('I\'m confused, too many rolls')
+            
             else:
-                await ctx.send('I\'m confused, too many rolls')
-        else:
-            if diceSides == 1:
-                message = 'Is this a joke? I don\'t have any mobius strip'
-            elif diceSides == 2:
-                message = 'Hmm, use a coin instead'
+                if diceSides == 1:
+                    message = 'Is this a joke? I don\'t have any mobius strip'
+                
+                elif diceSides == 2:
+                    message = 'Hmm, use a coin instead'
+                
+                else:
+                    message = 'Wait, what?'
+                
+                await ctx.send(message)
+    
+    @commands.command()
+    async def bored(
+            self, ctx,
+            participants: typing.Optional[typing.Union[int, str]],
+            *, _
+            ):
+        """
+        Feel bored? Find new activity
+        
+        ***Usage***: `[p]bored [participants]`
+        ***Example***: `{0}bored` or `{0}bored 3`
+        """
+        async with ctx.typing():
+            activity = m_bored.get_activity(participants=participants)
+            link = f"\nLink: {activity.link}" if activity.link != "" else ""
+            embed = discord.Embed(
+                title=activity.activity,
+                description=f"Participants: {activity.participants}\n"
+                            f"Type: {activity.type}" + link
+                )
+        await ctx.send(embed=embed)
+    
+    @commands.command()
+    async def agify(
+            self, ctx,
+            *, name: typing.Optional[typing.Union[str, discord.User]] = None
+            ):
+        """
+        I'll try to predict your age from your name
+
+        ***Usage***: `[p]agify <name>`
+
+        ***Note***:
+        ___name___: should only consist of alphabet (which means no space or symbols)
+
+        ***Example***:
+        `ketan.agify` which means predict my age from my discord name
+        `ketan.agify JohnDoe` wich means predict my age from my real name (John Doe)
+        """
+        async with ctx.typing():
+            if not name:
+                name = ctx.author.display_name
+            elif isinstance(name, discord.User):
+                name = name.display_name
+            name = name.replace(" ", "")
+            result = m_age_nation.get_age(name=name)
+            embed = discord.Embed(
+                title=f"Age prediction for \"{name}\"",
+                description=f"Age: {result}"
+                )
+        await ctx.send(embed=embed)
+    
+    @commands.command()
+    async def nationalize(
+            self, ctx,
+            *, name: typing.Optional[typing.Union[str, discord.User]] = None
+            ):
+        """
+        I'll try to predict your nationality from your name
+
+        ***Usage***: `[p]nationalize <name>`
+
+        ***Note***:
+        ___name___: should only consist of alphabet (which means no space or symbols)
+
+        ***Example***:
+        `ketan.nationalize` which means predict my nationality from my discord name
+        `ketan.nationalize JohnDoe` which means predict my nationality from my real name (John Doe)
+        """
+        async with ctx.typing():
+            if not name:
+                name = ctx.author.display_name
+            elif isinstance(name, discord.User):
+                name = name.display_name
+            name = name.replace(" ", "")
+            result = m_age_nation.get_nation(name=name)
+            embed = discord.Embed(
+                title=f"Nationality prediction for \"{name}\"",
+                description="Prediction Result:"
+                )
+            if len(result) > 0:
+                for nation in result:
+                    embed.add_field(
+                        name=nation.country_id,
+                        value=nation.probability
+                        )
             else:
-                message = 'Wait, what?'
-            await ctx.send(message)
+                embed.add_field(
+                    name="Can't predict",
+                    value="Try other name"
+                    )
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
